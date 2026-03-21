113c8864569323eadb81497ba160a9f9da7c5353
ehale8 <evielouhale@gmail.com>
2026-03-21 11:02:34 -0500

    gitignore

--- src/image.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-import java.awt.*;
-public class image extends JPanel {
-    public void paintComponent(Graphics g) {
-        ImageIcon image = new ImageIcon("\"C:\\Users\\eviel\\Downloads\\IMG_7900.jpeg\"");
-        g.drawImage(image.getImage(), 300, 300, 44, 24, null);
-
-    }
-}
