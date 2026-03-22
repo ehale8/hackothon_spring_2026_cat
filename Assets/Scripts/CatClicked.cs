e18eab329a3b1758e2ba0b18ecbb9250615ac64f
ehale8 <evielouhale@gmail.com>
2026-03-21 23:37:06 -0500

    start page

---
+++ Assets/Scripts/CatClicked.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CatClicked : MonoBehaviour
+{
+
+    [SerializeField] private string SceneName = "WinScene";
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Cat found! Loading win scene...");
+        SceneManager.LoadScene(SceneName);
+    }
+}
