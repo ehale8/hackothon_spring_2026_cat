4f42a1fb9b176f536a6d60f6707bb892306f5e24
ehale8 <evielouhale@gmail.com>
2026-03-22 09:45:13 -0500

    Revert "Merge branch 'Sprite_Animation' into help"
    This reverts commit 705e27ce7052f1ea44004b2a0043d3f2b7c99ce1, reversing
    changes made to 9c2bd0624ec7521ad3ed5d243fa424c9e9f49af0.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,23 +10,16 @@ public class Player : MonoBehaviour
     public Vector2 moveInput;
 
     public Animator animator; //Added later.
-    public SpriteRenderer sprite; //Added later.
+    public SpriteRenderer sprite;
 
 
     void FixedUpdate()
     {
         float targetSpeed = moveInput.x * speed;
         rb.linearVelocity = new Vector2 (targetSpeed, rb.linearVelocity.y);
-
-        sprite = GetComponent<SpriteRenderer>();
-        moveInput.x = Input.GetAxisRaw("Horizontal");
         if (targetSpeed != 0)
         {
             animator.SetBool("OntheMove", true);
-            if (moveInput.x < 0)
-            {
-              animator.SetBool("Facing_Left", true);
-            } else { animator.SetBool("Facing_Left", false); }
         } else { animator.SetBool("OntheMove", false); }
      
     }
